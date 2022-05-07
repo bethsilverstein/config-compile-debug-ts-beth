452710dd319328c3f9b4f3841e45492f9b88b76b
bethsilverstein <beth.silverstein@tradeshift.com>
2022-05-07 17:28:01 -0500

    use webpack to generate single file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // Comments baby!!!
+//const Main = require("./app/Main");
+import { Main } from "./app/Main";
 
 const funMessage : string = 'aloha';
 
 console.log(funMessage);
+
+new Main().render();
