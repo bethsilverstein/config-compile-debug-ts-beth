a9b858e102566987eb7450f684b5c62abba2ff98
bethsilverstein <beth.silverstein@tradeshift.com>
2022-08-18 18:53:17 -0500

    add not fun message

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Main_1 = require("./app/Main");
-const funMessage = 'aloha';
-console.log(funMessage);
-new Main_1.Main().render();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+/* eslint-disable semi */
 // Comments baby!!!
-//const Main = require("./app/Main");
-import { Main } from "./app/Main";
+// const Main = require("./app/Main");
+import { Main } from './app/Main';
 
 const funMessage : string = 'aloha';
+const notFunMessage : string = 'peace out';
 
 console.log(funMessage);
+console.log(notFunMessage);
 
 new Main().render();
